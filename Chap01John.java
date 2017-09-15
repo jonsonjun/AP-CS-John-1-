5c8d8a4f6740f1bdd86b7dccf795d9cfac058eef
jonsonjun <31872975+jonsonjun@users.noreply.github.com>
2017-09-15 16:19:07 +0800

    Add files via upload

---
+++ Chap01John.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package jonsonjun;
+
+/**
+ * @author lenovo
+ *
+ */
+public class Chap01John {
+	public static void main (String [] args) {
+	// generate some simple output
+	      System.out.print("1.4 Class, method, statement and comment\nHello, World");
+	      System.out.println("\n\n1.5\"print\" versus \"println\"\nHello, World!\nThis example also shows the escape sequence \" \\\"\"");
+	      System.out.println("\n1.6.1 The escape sequence \"\\n\"|\n(1) Hello,\nWorld!\n(2) Hello,/nWorld!");
+	      System.out.println("\n1.6.2 The escape sequence :\"\\t\"\nHello,  World£¡¡¢\nHello,\n\tWorld!\n(1)Hello,\tWorld!\n(2)Hello,\n\tWorld!\n\n(5) He said:\"Hello, World!\"\n(6) Hello, World!\\\\");
+	}
+}
